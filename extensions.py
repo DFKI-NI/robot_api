ac83cb128bc35c38df74ce8ec764aa31a0b86f88
Alexander Sung <alexander.sung@dfki.de>
2021-11-12 12:34:39 +0100

    generalize moveit actions, add ft_observer

--- extensions.py
+++ extensions.py
@@ -2,7 +2,8 @@
 from typing import Any, Callable, Optional
 import rospy
 import actionlib
-from mobipick_pick_n_place.msg import MoveItMacroAction, MoveItMacroGoal, MoveItMacroResult
+from mobipick_pick_n_place.msg import MoveItMacroAction, MoveItMacroGoal, MoveItMacroResult, \
+    FtObserverAction, FtObserverGoal
 from robot_api.lib import execute_once
 
 
@@ -10,26 +11,48 @@ class Arm:
     def __init__(self, namespace: str) -> None:
         self._moveit_macros_action_client = actionlib.SimpleActionClient(namespace + "moveit_macros",
             MoveItMacroAction)
+        self._ft_observer_action_client = actionlib.SimpleActionClient(namespace + "ft_observer", FtObserverAction)
 
     def _connect_moveit_macros(self) -> Any:
         rospy.logdebug("Waiting for moveit_macros action server ...")
         return self._moveit_macros_action_client.wait_for_server()
 
-    def move(self, goal_name: str, goal_type: str="target",
+    def _connect_ft_observer(self) -> Any:
+        rospy.logdebug("Waiting for ft_observer action server ...")
+        return self._ft_observer_action_client.wait_for_server()
+
+    def execute(self, action_name: str, action_type: str="function",
             done_cb: Optional[Callable[[int, MoveItMacroResult], Any]]=None) -> Any:
-        """Move arm to goal_name if goal_type is 'target', or call goal_name if goal_type is 'function'.
-        Optionally, call done_cb() afterwards if given.
-        """
-        goal = MoveItMacroGoal()
-        goal.type = goal_type
-        goal.name = goal_name
+        """Execute moveit_macro of action_type with action_name. Optionally, call done_cb() afterwards if given."""
         if not execute_once(self._connect_moveit_macros):
-            rospy.logerr(f"Cannot move arm to goal.{' ROS is shutting down.' if rospy.is_shutdown() else ''}")
+            rospy.logerr(f"Cannot execute MoveIt action.{' ROS is shutting down.' if rospy.is_shutdown() else ''}")
             return None
 
-        rospy.logdebug(f"Sending moveit_macro goal '{goal_name}' ...")
-        # Note: http://docs.ros.org/en/kinetic/api/actionlib_msgs/html/msg/GoalStatus.html
+        goal = MoveItMacroGoal()
+        goal.type = action_type
+        goal.name = action_name
         if done_cb is None:
             return self._moveit_macros_action_client.send_goal_and_wait(goal)
         else:
             return self._moveit_macros_action_client.send_goal(goal, done_cb)
+
+    def move(self, goal_name: str, done_cb: Optional[Callable[[int, MoveItMacroResult], Any]]=None) -> Any:
+        """Move arm to pose with goal_name. Optionally, call done_cb() afterwards if given."""
+        rospy.logdebug(f"Sending moveit_macro goal '{goal_name}' ...")
+        return self.execute(goal_name, "target", done_cb)
+
+    def get_result(self, action_name: str, done_cb: Optional[Callable[[int, MoveItMacroResult], Any]]=None) -> Any:
+        """Execute action_name and return its result (not state). Optionally, call done_cb() afterwards if given."""
+        self.execute(action_name, "function", done_cb)
+        return self._moveit_macros_action_client.get_result()
+
+    def observe_user_interaction(self) -> Any:
+        """Observe and return state."""
+        execute_once(self._connect_ft_observer)
+        goal = FtObserverGoal()
+        goal.threshold = 5.0
+        goal.timeout = 30
+        self._ft_observer_action_client.send_goal(goal)
+        self._ft_observer_action_client.wait_for_result()
+        # Note: http://docs.ros.org/en/kinetic/api/actionlib_msgs/html/msg/GoalStatus.html
+        return self._ft_observer_action_client.get_state() == 3

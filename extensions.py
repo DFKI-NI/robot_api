96e10f1aec5c9b449911d094d8daf36166e2c9d2
Alexander Sung <alexander.sung@dfki.de>
2021-11-12 17:51:47 +0100

    refactor extensions to register action clients to available servers

--- extensions.py
+++ extensions.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python3
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Dict, Optional
 import rospy
+import genpy
 import actionlib
 from mobipick_pick_n_place.msg import MoveItMacroAction, MoveItMacroGoal, MoveItMacroResult, \
     FtObserverAction, FtObserverGoal
 from robot_api.lib import execute_once
 
 
-class Arm:
+class Extension:
+    def __init__(self, namespace: str, server_specs: Dict[str, genpy.Message], connect_on_init: bool=False) -> None:
+        self._action_clients = {}  # type: Dict[str, actionlib.SimpleActionClient]
+        # Add clients to all action servers in server_specs found by published ROS topics.
+        for server_name, action_spec in server_specs.items():
+            if self._is_topic_of_type(namespace, server_name + "/result", action_spec.__name__ + "Result"):
+                action_client = actionlib.SimpleActionClient(namespace + server_name, action_spec)
+                # Note: server_name is a key in server_specs and thus unique.
+                self._action_clients[server_name] = action_client
+                if connect_on_init:
+                    self._connect(server_name)
+
+    def _connect(self, server_name: str) -> Any:
+        rospy.logdebug(f"Waiting for {server_name} action server ...")
+        return self._action_clients[server_name].wait_for_server()
+
+    def _is_topic_of_type(self, namespace: str, topic: str, message_type: str) -> bool:
+        """Return whether topic is published in namespace using message_type."""
+        for check_topic, check_message_type in rospy.get_published_topics(namespace):
+            if check_topic == namespace + topic and check_message_type.split('/')[-1] == message_type:
+                return True
+        return False
+
+
+class Arm(Extension):
     def __init__(self, namespace: str, connect_manipulation_on_init: bool) -> None:
-        self._moveit_macros_action_client = actionlib.SimpleActionClient(namespace + "moveit_macros",
-            MoveItMacroAction)
-        self._ft_observer_action_client = actionlib.SimpleActionClient(namespace + "ft_observer", FtObserverAction)
-        if connect_manipulation_on_init:
-            execute_once(self._connect_moveit_macros)
-            execute_once(self._connect_ft_observer)
-
-    def _connect_moveit_macros(self) -> Any:
-        rospy.logdebug("Waiting for moveit_macros action server ...")
-        return self._moveit_macros_action_client.wait_for_server()
-
-    def _connect_ft_observer(self) -> Any:
-        rospy.logdebug("Waiting for ft_observer action server ...")
-        return self._ft_observer_action_client.wait_for_server()
+        super().__init__(namespace, {
+            "moveit_macros": MoveItMacroAction,
+            "ft_observer": FtObserverAction
+        }, connect_manipulation_on_init)
 
     def execute(self, action_name: str, action_type: str="function",
             done_cb: Optional[Callable[[int, MoveItMacroResult], Any]]=None) -> Any:
         """Execute moveit_macro of action_type with action_name. Optionally, call done_cb() afterwards if given."""
-        if not execute_once(self._connect_moveit_macros):
+        if not self._connect("moveit_macros"):
             rospy.logerr(f"Cannot execute MoveIt action.{' ROS is shutting down.' if rospy.is_shutdown() else ''}")
             return None
 
@@ -35,9 +50,9 @@ class Arm:
         goal.type = action_type
         goal.name = action_name
         if done_cb is None:
-            return self._moveit_macros_action_client.send_goal_and_wait(goal)
+            return self._action_clients["moveit_macros"].send_goal_and_wait(goal)
         else:
-            return self._moveit_macros_action_client.send_goal(goal, done_cb)
+            return self._action_clients["moveit_macros"].send_goal(goal, done_cb)
 
     def move(self, goal_name: str, done_cb: Optional[Callable[[int, MoveItMacroResult], Any]]=None) -> Any:
         """Move arm to pose with goal_name. Optionally, call done_cb() afterwards if given."""
@@ -47,15 +62,16 @@ class Arm:
     def get_result(self, action_name: str, done_cb: Optional[Callable[[int, MoveItMacroResult], Any]]=None) -> Any:
         """Execute action_name and return its result (not state). Optionally, call done_cb() afterwards if given."""
         self.execute(action_name, "function", done_cb)
-        return self._moveit_macros_action_client.get_result()
+        return self._action_clients["moveit_macros"].get_result()
 
     def observe_user_interaction(self) -> Any:
         """Observe and return state."""
-        execute_once(self._connect_ft_observer)
+        self._connect("ft_observer")
         goal = FtObserverGoal()
         goal.threshold = 5.0
         goal.timeout = 30
-        self._ft_observer_action_client.send_goal(goal)
-        self._ft_observer_action_client.wait_for_result()
+        action_client = self._action_clients["ft_observer"]
+        action_client.send_goal(goal)
+        action_client.wait_for_result()
         # Note: http://docs.ros.org/en/kinetic/api/actionlib_msgs/html/msg/GoalStatus.html
-        return self._ft_observer_action_client.get_state() == 3
+        return action_client.get_state() == 3

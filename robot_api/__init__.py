41687a48af4e69a220f5d625813f4e9278ac0548
Alexander Sung <alexander.sung@dfki.de>
2021-11-25 10:06:31 +0100

    expose Actions on package API

--- robot_api/__init__.py
+++ robot_api/__init__.py
@@ -1,3 +1,5 @@
 from robot_api.lib import find_robot_namespaces, Action
-from robot_api.core import Robot
+from robot_api.core import Robot, BaseMoveToGoalAction, BaseMoveToPoseAction, BaseMoveToPositionOrientationAction, \
+    BaseMoveToCoordinatesAction
+from robot_api.extensions import ArmMoveItMacroAction, ArmForceTorqueObserverAction
 from robot_api.excepthook import Excepthook
